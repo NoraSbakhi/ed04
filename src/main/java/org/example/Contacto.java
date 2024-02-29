309dda17a195aa6ef7c02d6d50289e2746e174a6
NoraSbakhi <manuelred@hotmail.com>
2024-02-29 14:48:11 +0100

    commit con comentarios JavaDoc ya realizados y subidos.

--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -6,24 +6,30 @@ import java.util.List;
 /** esta clase  contiende dos atributos: el nombre del contacto y un listado de teléfonos asociados a dicho contacto.
  *  * @author NoraSbakhi
  *  * version 1.0 29/02/2024*/
-class Contacto
+public class Contacto
 {
     private String name;
     private List<String> phones;
-/** este es el atributo contacto*/
+/** constructos para la clase contacto, asigna un atributo nombre y teléfono
+ * primero asigan el nombre al atributo noame, luego inicializa el atributo
+ * como una nueva instancia y fianlemnte agrega el numeor de telfono proporcionado a la lista phones
+ * @param name
+ *  @param phone */
+
     public Contacto(String name, String phone)
+
     {
         this.name = name;
-        this.phones = new ArrayList<>(); /** eso es una lista de telefonos asociados al contacto*/
+        this.phones = new ArrayList<>(); // inicializa el aributo phones como una nueva instande de ArrayList
         this.phones.add(phone);
     }
-
-    public String getName() /** @return método que devuelve el nombre del contacto*/
+    /** @return método que devuelve el nombre del contacto*/
+    public String getName()
     {
         return this.name;
     }
-
-    public List<String> getPhones() /** @return método que devuelve el numero de telefono del contacto*/
+    /** @return método que devuelve el numero de telefono del contacto*/
+    public List<String> getPhones()
     {
         return this.phones;
     }
